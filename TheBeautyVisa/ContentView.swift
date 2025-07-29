621993087c454a98fb047199ac63b5c9561c0604
Linh Vo <hallinhvo30112008@gmail.com>
2025-07-29 10:51:19 -0400

    Created base of the BeautyVisa app

--- TheBeautyVisa/ContentView.swift
+++ TheBeautyVisa/ContentView.swift
@@ -1,21 +1,51 @@
-//
-//  ContentView.swift
-//  TheBeautyVisa
-//
-//  Created by Scholar on 7/29/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+        NavigationStack {
+            VStack {
+                Image("BeautyVisaLogo")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                
+                Text("BEAUTY VISA")
+                    .font(.title)
+                    .fontWeight(.bold)
+              
+                Text("Beauty Visa delivers personalized skincare and makeup tips based on your location or travel plans, factoring in climate, local beauty culture, and ingredient access.")
+                    .multilineTextAlignment(.center)
+                
+                Text("Choose a region below to start!")
+                    .font(.headline)
+                                                    
+                Divider()
+                
+                Text("Regions")
+                    .font(.system(size: 30))
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    
+                
+                HStack(spacing: 20) {
+                    NavigationLink("Region 1", destination: SecondView())
+//                    NavigationLink("Region 2", destination: ThirdView())
+                    NavigationLink("Region 3", destination: SecondView())
+                }
+                .padding()
+                
+                HStack(spacing: 20) {
+                    NavigationLink("Region 4", destination: SecondView())
+                    NavigationLink("Region 5", destination: SecondView())
+                    NavigationLink("Region 6", destination: SecondView())
+                }
+                .padding()
+                
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
