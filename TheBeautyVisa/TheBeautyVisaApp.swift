da399918f9f6f1359a8c88ed1d0dfad5edb573e7
Linh Vo <hallinhvo30112008@gmail.com>
2025-07-29 10:44:24 -0400

    Initial Commit

---
+++ TheBeautyVisa/TheBeautyVisaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheBeautyVisaApp.swift
+//  TheBeautyVisa
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct TheBeautyVisaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
