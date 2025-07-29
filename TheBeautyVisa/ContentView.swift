7cc2ac4fcd2f2eeed9bd51c5d95cdfe7c39a042f
Miley <cutegoldendragon@gmail.com>
2025-07-29 11:18:52 -0400

    customized the buttons for each continent

--- TheBeautyVisa/ContentView.swift
+++ TheBeautyVisa/ContentView.swift
@@ -12,14 +12,17 @@ struct ContentView: View {
                     .clipShape(Circle())
                 
                 Text("BEAUTY VISA")
-                    .font(.title)
+                    .font(.system(size: 37))
                     .fontWeight(.bold)
+                    .padding(.bottom, 10.0)
               
                 Text("Beauty Visa delivers personalized skincare and makeup tips based on your location or travel plans, factoring in climate, local beauty culture, and ingredient access.")
                     .multilineTextAlignment(.center)
+                    .padding(.bottom, 15.0)
                 
                 Text("Choose a region below to start!")
                     .font(.headline)
+                    .padding(.bottom, 10.0)
                                                     
                 Divider()
                 
@@ -31,15 +34,51 @@ struct ContentView: View {
                 
                 HStack(spacing: 20) {
                     NavigationLink("Asia", destination: SecondView())
+                        .frame(width: 100, height: 50)
+                        .frame(width: 100, height: 50)
+                                .background(Color(red: 227/255, green: 167/255, blue: 5/255))
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                                .fontWeight(.bold)
                     NavigationLink("Europe", destination: SecondView())
+                        .frame(width: 100, height: 50)
+                        .frame(width: 100, height: 50)
+                                .background(Color(red: 227/255, green: 167/255, blue: 5/255))
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                                .fontWeight(.bold)
                     NavigationLink("Australia", destination: SecondView())
+                        .frame(width: 100, height: 50)
+                        .frame(width: 100, height: 50)
+                                .background(Color(red: 227/255, green: 167/255, blue: 5/255))
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                                .fontWeight(.bold)
                 }
                 .padding()
                 
                 HStack(spacing: 20) {
                     NavigationLink("North America", destination: SecondView())
+                        .frame(width: 100, height: 50)
+                        .frame(width: 100, height: 50)
+                                .background(Color(red: 227/255, green: 167/255, blue: 5/255))
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                                .fontWeight(.bold)
                     NavigationLink("South America", destination: SecondView())
+                        .frame(width: 100, height: 50)
+                        .frame(width: 100, height: 50)
+                                .background(Color(red: 227/255, green: 167/255, blue: 5/255))
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                                .fontWeight(.bold)
                     NavigationLink("Africa", destination: SecondView())
+                        .frame(width: 100, height: 50)
+                        .frame(width: 100, height: 50)
+                                .background(Color(red: 227/255, green: 167/255, blue: 5/255))
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                                .fontWeight(.bold)
                 }
                 .padding()
                 
--- TheBeautyVisa/SecondView.swift
+++ TheBeautyVisa/SecondView.swift
@@ -10,12 +10,7 @@ import SwiftUI
 
 struct SecondView: View {
     var body: some View {
-        Text("Northern Europe")
-            .font(.system(size: 30))
-            .fontWeight(.semibold)
-            .multilineTextAlignment(.center)
-        Image("northerneurope")
-        Text("")
+Text("")
     }
 }
 
