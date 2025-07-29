0a1e1b27afb9d17f4f35ba1ab3382add44eeb826
Miley <cutegoldendragon@gmail.com>
2025-07-29 12:46:22 -0400

    added image, revised buttons

---
+++ TheBeautyVisa/ThirdView.swift
@@ -0,0 +1,18 @@
+//
+//  ThirdView.swift
+//  TheBeautyVisa
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+struct ThirdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThirdView()
+}
