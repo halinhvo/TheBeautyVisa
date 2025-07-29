d26283114e3e51a043d3f01f0385ef9096e87e1d
Linh Vo <hallinhvo30112008@gmail.com>
2025-07-29 11:31:48 -0400

    Homepage v2

--- TheBeautyVisa/ContentView.swift
+++ TheBeautyVisa/ContentView.swift
@@ -2,48 +2,90 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-            
-        NavigationStack {
-            VStack {
-                Image("BeautyVisaLogo")
-                    .resizable()
-                    .frame(width: 200, height: 200)
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(Circle())
-                
-                Text("BEAUTY VISA")
-                    .font(.title)
-                    .fontWeight(.bold)
-              
-                Text("Beauty Visa delivers personalized skincare and makeup tips based on your location or travel plans, factoring in climate, local beauty culture, and ingredient access.")
-                    .multilineTextAlignment(.center)
-                
-                Text("Choose a region below to start!")
-                    .font(.headline)
-                                                    
-                Divider()
-                
-                Text("Continents")
-                    .font(.system(size: 30))
-                    .fontWeight(.semibold)
-                    .multilineTextAlignment(.center)
+        ZStack {
+//            Color(red: 228/255, green: 91/255, blue: 59/255)
+//                .ignoresSafeArea()
+            NavigationStack {
+                VStack {
+                    Image("BeautyVisaLogo")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
                     
-                
-                HStack(spacing: 20) {
-                    NavigationLink("Asia", destination: SecondView())
-                    NavigationLink("Europe", destination: SecondView())
-                    NavigationLink("Australia", destination: SecondView())
+                    Text("BEAUTY VISA")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Text("Beauty Visa delivers personalized skincare and makeup tips based on your location or travel plans, factoring in climate, local beauty culture, and ingredient access.")
+                        .multilineTextAlignment(.center)
+                    
+                    Text("Choose a region below to start!")
+                        .font(.headline)
+                    
+                    Divider()
+                    
+                    Text("Continents")
+                        .font(.system(size: 30))
+                        .fontWeight(.semibold)
+                        .multilineTextAlignment(.center)
+                    
+                    
+                    HStack(spacing: 20) {
+                        NavigationLink("Asia", destination: SecondView())
+                            .frame(width: 150, height: 20)
+                            .padding()
+                            .background(Color(red: 227/255, green: 167/255, blue: 5/255))
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .font(.headline)
+                        NavigationLink("Europe", destination: SecondView())
+                            .frame(width: 150, height: 20)
+                            .padding()
+                            .background(Color(red: 227/255, green: 167/255, blue: 5/255))
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .font(.headline)
+                    }
+                    .padding()
+                    
+                    HStack(spacing: 20) {
+                        NavigationLink("North America", destination: SecondView())
+                            .frame(width: 150, height: 20)
+                            .padding()
+                            .background(Color(red: 227/255, green: 167/255, blue: 5/255))
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .font(.headline)
+                        NavigationLink("South America", destination: SecondView())
+                            .frame(width: 150, height: 20)
+                            .padding()
+                            .background(Color(red: 227/255, green: 167/255, blue: 5/255))
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .font(.headline)
+                    }
+                    .padding()
+                    
+                    HStack(spacing: 20) {
+                        NavigationLink("Africa", destination: SecondView())
+                            .frame(width: 150, height: 20)
+                            .padding()
+                            .background(Color(red: 227/255, green: 167/255, blue: 5/255))
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .font(.headline)
+                        NavigationLink("Australia", destination: SecondView())
+                            .frame(width: 150, height: 20)
+                            .padding()
+                            .background(Color(red: 227/255, green: 167/255, blue: 5/255))
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .font(.headline)
+                    }
+                    .padding()
+                    Spacer()
                 }
-                .padding()
-                
-                HStack(spacing: 20) {
-                    NavigationLink("North America", destination: SecondView())
-                    NavigationLink("South America", destination: SecondView())
-                    NavigationLink("Africa", destination: SecondView())
-                }
-                .padding()
-                
-                Spacer()
             }
         }
     }
