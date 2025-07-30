5b83e454ba8db18e56a2b3ef51af98435b56ff1e
Miley <cutegoldendragon@gmail.com>
2025-07-30 11:30:16 -0400

    southeast womens fashion started

--- TheBeautyVisa/SEAsiaWomensFashion.swift
+++ TheBeautyVisa/SEAsiaWomensFashion.swift
@@ -1,17 +1,23 @@
-//
-//  SEAsiaWomensFashion.swift
-//  TheBeautyVisa
-//
-//  Created by Scholar on 7/30/25.
-//
-
 import SwiftUI
 
 struct SEAsiaWomensFashion: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack(alignment: .bottom) {
+                Color(.systemBackground).ignoresSafeArea()
+                
+                ScrollView {
+                    VStack {
+                        Text("Women's Fashion")
+                            .fontWeight(.bold)
+                            .font(.title)
+                    }
+                }
+            }
+        }
     }
 }
+
 
 #Preview {
     SEAsiaWomensFashion()
