166043bc5f50c7ce61814b52f809d1bb0bb85069
Katelynn Le <katelynnle2010@gmail.com>
2025-07-31 10:12:08 -0400

    EAsia

---
+++ TheBeautyVisa/EAsiaBeauty.swift
@@ -0,0 +1,18 @@
+//
+//  EAsiaBeauty.swift
+//  TheBeautyVisa
+//
+//  Created by Scholar on 7/31/25.
+//
+
+import SwiftUI
+
+struct EAsiaBeauty: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EAsiaBeauty()
+}
